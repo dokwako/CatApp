ceac30a4efe1815a75629f32c346ffe422eb8461
dokwako <denzilwawire418@gmail.com>
2024-12-02 22:38:08 +0300

    initial

---
+++ app/src/main/java/com/example/catapp/core/NetworkResult.kt
@@ -0,0 +1,8 @@
+package com.example.catapp.core
+
+sealed class NetworkResult<out T> {
+    data class Success<out T>(val data: T) : NetworkResult<T>()
+    data class Error(val message: String) : NetworkResult<Nothing>()
+    object Loading: NetworkResult<Nothing>()
+
+}
