9a1b5e430da16745303e248953d95d8c34a3e82c
dokwako <denzilwawire418@gmail.com>
2024-12-02 16:30:23 +0300

    setting up compose to display cats

--- app/src/main/java/com/example/catapp/ui/screens/CatScreen.kt
+++ app/src/main/java/com/example/catapp/ui/screens/CatScreen.kt
@@ -1,7 +1,17 @@
 package com.example.catapp.ui.screens
 
+import android.net.Network
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import com.example.catapp.ui.viewmodel.CatViewModel
 
 @Composable
@@ -10,5 +20,28 @@ fun CatScreen(catViewModel: CatViewModel) {
 
     LaunchedEffect(Unit) {
         catViewModel.fetchCats()
+    }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        when (val result = catsState.value) {
+            is NetworkResult.Loading -> {
+                CircularProgressIndicator()
+            }
+            is NetworkResult.Success -> {
+                result.data?.ForEach { catImageUrl ->
+                    Image(
+                        painter = rememberImagePainter(catImageUrl),
+                        contentDescription = null,
+                        modifier = Modifier.height(200.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+            }
+            is NetworkResult.Error -> {
+                Text(text = "Error: ${result.message}")
+            }
+        }
+
     }
 }
