cc0772f0f46fe48c62a5bd73b9df0d9e3cf716fc
dokwako <denzilwawire418@gmail.com>
2024-12-02 10:45:42 +0300

    Adding dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,28 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    //Hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+
+    //Jetpack Compose
+    implementation("androidx.compose.ui:ui:1.5.0")
+    implementation("androidx.compose.material3:material3:1.0.1")
+    implementation("androidx.compose.ui:tooling-preview:1.5.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.1")
+
+    //Retrofit & Gson for API calls
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+
+
+    // Coroutine for background tasks
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
 }
 
