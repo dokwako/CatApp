4a3591a6276d5e339438ec3ee687aced289b97ab
dokwako <denzilwawire418@gmail.com>
2024-12-03 15:12:18 +0300

    catViewModel

--- app/src/main/java/com/example/catapp/ui/viewmodel/CatViewModel.kt
+++ app/src/main/java/com/example/catapp/ui/viewmodel/CatViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.catapp.core.NetworkResult
 import com.example.catapp.data.repository.CatRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -13,7 +14,7 @@ import javax.inject.Inject
 class CatViewModel @Inject constructor(
     private val catRepository: CatRepository
 ) : ViewModel() {
-    private val _cats = mutableStateOf<List<String>>(NetworkResult.Loading())
+    private val _cats = mutableStateOf<NetworkResult<List<String>>>(NetworkResult.Loading())
     val cats: State<NetworkResult<List<String>>> get()= _cats
 
     //Fetch cat data
