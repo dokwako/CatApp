32a8a7eb29ca63795cfdc1f933c48cdfa89e4a0e
dokwako <denzilwawire418@gmail.com>
2024-12-09 18:03:45 +0300

    data fetching from network

--- app/src/main/java/com/example/catapp/data/repository/CatRepository.kt
+++ app/src/main/java/com/example/catapp/data/repository/CatRepository.kt
@@ -1,29 +1,25 @@
 package com.example.catapp.data.repository
 
-import android.net.wifi.WifiManager.AddNetworkResult
 import com.example.catapp.core.NetworkResult
 import com.example.catapp.data.api.CatApiService
-import okhttp3.Response
 import javax.inject.Inject
 
 class CatRepository @Inject constructor(
-    private val CatApiService: CatApiService
+    private val catApiService: CatApiService // Corrected naming
 ) {
 
-    //fetch cat images from the Api
-    suspend fun getCats(): NetworkResult<List<String>> {
+    // Fetch cat images from the API
+    suspend fun getCats(apiKey: String): NetworkResult<List<String>> {
         return try {
-            val response = CatApiService.getCatImages()
+            val response = catApiService.getCatImages(apiKey = apiKey)
             if (response.isSuccessful && response.body() != null) {
                 val imageUrls = response.body()!!.map { it.url }
                 NetworkResult.Success(imageUrls)
             } else {
                 NetworkResult.Error("Error: ${response.message()}")
             }
-
         } catch (e: Exception) {
             NetworkResult.Error("Failed to load cats: ${e.message}")
         }
-
     }
 }
