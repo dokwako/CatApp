e85c66a49174e5685bc0f8e69c3d92768e1ad4ea
dokwako <denzilwawire418@gmail.com>
2025-01-26 23:52:19 +0300

    CatScreen

--- app/src/main/java/com/example/catapp/ui/presentation/CatScreen.kt
+++ app/src/main/java/com/example/catapp/ui/presentation/CatScreen.kt
@@ -1,10 +1,10 @@
 package com.example.catapp.ui.presentation
 
-
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,10 +13,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import com.example.catapp.ui.viewmodel.CatViewModel
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.example.catapp.ui.viewmodel.CatViewModel
 import com.example.catapp.core.NetworkResult
-
+import androidx.compose.foundation.shape.RoundedCornerShape
 
 @Composable
 fun CatScreen(catViewModel: CatViewModel) {
@@ -26,26 +27,55 @@ fun CatScreen(catViewModel: CatViewModel) {
         catViewModel.fetchCats()
     }
 
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        when (val result = catsState.value) {
-            is NetworkResult.Loading -> {
+    when (val result = catsState.value) {
+        is NetworkResult.Loading -> {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
                 CircularProgressIndicator()
             }
-            is NetworkResult.Success -> {
-                result.data?.forEach { catImageUrl ->
-                    AsyncImage(
-                        model = catImageUrl,
-                        contentDescription = null,
-                        modifier = Modifier.height(200.dp),
-                        contentScale = ContentScale.Crop
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
+        }
+        is NetworkResult.Success -> {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2), // 2 columns in the grid
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(result.data) { catImageUrl ->
+                    CatCard(catImageUrl)
                 }
             }
-            is NetworkResult.Error -> {
+        }
+        is NetworkResult.Error -> {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
                 Text(text = "Error: ${result.message}")
             }
         }
+    }
+}
 
+@Composable
+fun CatCard(catImageUrl: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f), // Ensures cards are square
+        shape = RoundedCornerShape(16.dp),
+        colors = androidx.compose.material3.CardDefaults.cardColors( // Optional, for color customization
+            containerColor = androidx.compose.material3.MaterialTheme.colorScheme.surface
+        )
+
+    ) {
+        AsyncImage(
+            model = catImageUrl,
+            contentDescription = "Cat Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
