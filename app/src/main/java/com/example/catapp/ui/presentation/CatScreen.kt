97308b5f5e9f81b5042173b8c1ab114859e80546
dokwako <denzilwawire418@gmail.com>
2025-02-23 10:50:43 +0300

    Update on CatScreen.kt

--- app/src/main/java/com/example/catapp/ui/presentation/CatScreen.kt
+++ app/src/main/java/com/example/catapp/ui/presentation/CatScreen.kt
@@ -1,15 +1,21 @@
 package com.example.catapp.ui.presentation
 
+import android.graphics.Paint.Align
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -22,8 +28,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
@@ -68,13 +77,33 @@ fun TopBar() {
        modifier = Modifier
            .fillMaxWidth()
            .padding(16.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
     ) {
-        Text(text = "Nairobi ,Kenya" , style = MaterialTheme.typography.titleMedium)
+        Row(verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.weight(1f)
+        ) {
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = "Location Icon",
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Nairobi ,Kenya" ,
+                style = MaterialTheme.typography.titleMedium,
+
+                )
+
+        }
+
         Image(
             painter = painterResource(id = R.drawable.profile),
             contentDescription = "Profile",
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
     }
 }
@@ -96,7 +125,7 @@ fun SearchBar(searchText: String, onSearchTextChange: (String) -> Unit) {
 }
 
 @Composable
-fun CatItem(Cat:Cat) {
+fun CatItem(cat:Cat) {
     Row (
         modifier =Modifier
             .fillMaxWidth()
